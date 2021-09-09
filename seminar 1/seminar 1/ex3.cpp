caddad3ddbcb9b370c5891c08d07f84b598f76a6
AntonVasin26 <vasin.anton.work@gmail.com>
2021-09-09 18:55:09 +0300

    18:54 09.09.21

--- seminar 1/seminar 1/ex3.cpp
+++ seminar 1/seminar 1/ex3.cpp
@@ -19,14 +19,32 @@ int main()
 	std::cout << "factors C = ";
 	std::cin >> c;
 
-	D = std::pow(b, 2) - 4*a*c;
+	D = std::pow(b, 2) - 4.0*a*c;
 
 	std::cout << '\n' << "*************** OUTPUT ***************" << "\n\n";
 	std::cout.precision(8);
 
-	if (std::abs(D) < epsilon)
+	if (std::abs(a) < epsilon)
 	{
-		std::cout << "the equation has one solution\n" << std::fixed << std::showpos << (-b / (2 * a));
+		if (std::abs(b) < epsilon)
+		{
+			if (std::abs(c) < epsilon)
+			{
+				std::cout << "the entire numerical line is the solution of the equation";
+			}
+			else
+			{
+				std::cout << "the equation has no solution\n";
+			}
+		}
+		else
+		{
+			std::cout << "the equation has one solution\n" << std::fixed << std::showpos << (-c / b);
+		}
+	}
+	else if (std::abs(D) < epsilon)
+	{
+		std::cout << "the equation has one solution\n" << std::fixed << std::showpos << (-b / (2.0 * a));
 	}
 	else if (D < 0)
 	{
@@ -35,8 +53,8 @@ int main()
 	else
 	{
 		std::cout << "the equation has two solution";
-		std::cout << "\nx1 = " << std::fixed << std::showpos << ((-b - std::sqrt(D)) / (2 * a));
-		std::cout << "\nx2 = " << std::fixed << std::showpos << ((-b + std::sqrt(D)) / (2 * a));
+		std::cout << "\nx1 = " << std::fixed << std::showpos << ((-b - std::sqrt(D)) / (2.0 * a));
+		std::cout << "\nx2 = " << std::fixed << std::showpos << ((-b + std::sqrt(D)) / (2.0 * a));
 	}
 	std::cout << "\n\n";
 
