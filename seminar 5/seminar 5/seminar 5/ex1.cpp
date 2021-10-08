5e7c2c217c16301c14c8f24a27c2a522d4bc9086
AntonVasin26 <vasin.anton.work@gmail.com>
2021-10-08 16:20:34 +0300

    sem5 ver4.1

--- seminar 5/seminar 5/seminar 5/ex1.cpp
+++ seminar 5/seminar 5/seminar 5/ex1.cpp
@@ -5,17 +5,17 @@ void ex1()
 {
 	std::cout << "\nex1 start\n";
 
-	Spaceship Borey("Borey", 100, 1000, 2000, 6, 400);
-	Borey.condition();
-	Borey.add_passenger("Anton", 90);
-	Borey.add_luggage("laptop", 2);
-	Borey.fill_up_energy(2300);
-	Borey.fill_up_fuel(200);
-	Borey.condition();
-	Borey.move(100);
-	Borey.fill_up_fuel(900);
-	Borey.move(100);
-	Borey.get_luggage("laptop");
-	Borey.get_luggage("mouse");
+	Spaceship borey("Borey", 100, 1000, 2000, 6, 400);
+	borey.condition();
+	borey.add_passenger("Anton", 90);
+	borey.add_luggage("laptop", 2);
+	borey.fill_up_energy(2300);
+	borey.fill_up_fuel(200);
+	borey.condition();
+	borey.move(100);
+	borey.fill_up_fuel(900);
+	borey.move(100);
+	borey.get_luggage("laptop");
+	borey.get_luggage("mouse");
 	std::cout << "\n\nex1 end\n";
 }
