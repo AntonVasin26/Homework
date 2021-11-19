9dfb5b6a71eae46c15455fc3fc29e22270fb2097
AntonVasin26 <vasin.anton.work@gmail.com>
2021-11-20 01:16:51 +0300

    Update ex2.cpp

--- seminar 11/seminar 11/ex2.cpp
+++ seminar 11/seminar 11/ex2.cpp
@@ -1,24 +1,26 @@
 #include <iostream>
 #include <type_traits>
 
+
+
 template < typename T >
 struct remove_const
 {
 	using type = T;
 };
 
-//template < typename T >
-//struct remove_const < const T >
-//{
-//	using type = T;
-//};
+template < typename T >
+struct remove_const < const T >
+{
+	using type = T;
+};
 
 //template < typename T >
 //struct remove_const < const T& >
 //{
 //	using type = T&;
 //};
-
+//
 //template < typename T >
 //struct remove_const < const T&& >
 //{
@@ -31,19 +33,20 @@ struct add_const
 	using type = const T;
 };
 
-template < typename T >
-struct add_const < T& >
-{
-	using type = const T&;
-};
+//template < typename T >
+//struct add_const < T& >
+//{
+//	using type = const T&;
+//};
+//
+//template < typename T >
+//struct add_const < T&& >
+//{
+//	using type = const T&&;
+//};
 
-template < typename T >
-struct add_const < T&& >
-{
-	using type = const T&&;
-};
-
-
+template <class _Ty>
+using remove_const_t = typename remove_const<_Ty>::type;
 
 template<typename T1, typename T2>
 void print_is_same() {
@@ -58,11 +61,12 @@ int main()
 	print_is_same<int&, const int&>();
 	print_is_same<int&&, const int&&>();
 	std::cout << "----------------------------------------\n";
-	//print_is_same<int, const int>();
-	//print_is_same<int, remove_const<const int>::type>();
-	//print_is_same<int&, remove_const<const int&>::type>();
-	//print_is_same<int&&, remove_const<const int&&>::type>();
-	print_is_same<int*& const , remove_const<int*&>::type>();
+	print_is_same<int, const int>();
+	print_is_same<int, remove_const<const int>::type>();
+	print_is_same<int, remove_const_t<const int>>();
+	print_is_same<int&, remove_const<const int&>::type>();
+	print_is_same<int&&, remove_const<const int&&>::type>();
+	print_is_same<int* , std::remove_const<int*const>::type>();
 	std::cout << "----------------------------------------\n";
 	print_is_same<const int, int>();
 	print_is_same<const int, add_const<int>::type>();
