4b56da85a5e9e55723a4f2227ed57d28ad43eb3e
AntonVasin26 <vasin.anton.work@gmail.com>
2021-11-19 17:41:46 +0300

    add ex2.cpp

---
+++ seminar 11/seminar 11/ex2.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <type_traits>
+
+template < typename T >
+struct remove_const
+{
+	using type = T;
+};
+
+template < typename T >
+struct remove_const < const T >
+{
+	using type = T;
+};
+
+template < typename T >
+struct remove_const < const T& >
+{
+	using type = T&;
+};
+
+template < typename T >
+struct remove_const < const T&& >
+{
+	using type = T&&;
+};
+
+template < typename T >
+struct add_const
+{
+	using type = const T;
+};
+
+template < typename T >
+struct add_const < T& >
+{
+	using type = const T&;
+};
+
+template < typename T >
+struct add_const < T&& >
+{
+	using type = const T&&;
+};
+
+template<class T1, class T2>
+void print_is_same() {
+	std::cout << std::is_same<T1, T2>() << '\n';
+}
+
+int main() 
+{
+	std::cout << std::boolalpha;
+	print_is_same<int, int>();
+	print_is_same<int, const int>();
+	print_is_same<int&, const int&>();
+	print_is_same<int&&, const int&&>();
+	std::cout << "----------------------------------------\n";
+	print_is_same<int, const int>();
+	print_is_same<int, remove_const<const int>::type>();
+	print_is_same<int&, remove_const<const int&>::type>();
+	print_is_same<int&&, remove_const<const int&&>::type>();
+	std::cout << "----------------------------------------\n";
+	print_is_same<const int, int>();
+	print_is_same<const int, add_const<int>::type>();
+	print_is_same<const int&, add_const<int&>::type>();
+	print_is_same<const int&&, add_const<int&&>::type>();
+}
+
